fef3e4b3916eeda0717e514f0161f42d7bf7682a
Joaquin Claren <joaquin.claren@gmail.com>
2021-06-01 08:40:42 -0400

    Agrega el adapter

--- app/src/main/java/adapters/RobotAdapter.java
+++ app/src/main/java/adapters/RobotAdapter.java
@@ -1,4 +1,43 @@
 package adapters;
 
-public class RobotAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import java.util.List;
+import  com.example.robots.R;
+import models.Robot;
+
+public class RobotAdapter extends ArrayAdapter<Robot> {
+    Context context;
+    private class ViewHolder {
+        TextView phone;
+        TextView nickname;
+
+        private ViewHolder() {
+        }
+    }
+    public RobotAdapter(Context context, List<Robot> items) {
+        super(context, 0, items);
+        this.context = context;
+    }
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        final Robot rowItem = (Robot) getItem(position);
+        LayoutInflater mInflater = (LayoutInflater) this.context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.robot_item, null);
+            holder = new ViewHolder();
+            holder.phone = (TextView) convertView.findViewById(R.id.phone);
+            holder.nickname = (TextView) convertView.findViewById(R.id.nickname);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.phone.setText(rowItem.phone);
+        holder.nickname.setText(rowItem.nickname);
+        return convertView;
+    }
 }
