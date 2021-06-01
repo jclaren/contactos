3b05fc6f564b715ccca45ec131a7a4daf8a29a13
Joaquin Claren <joaquin.claren@gmail.com>
2021-06-01 10:21:21 -0400

    Muestra contactos de jsonplaceholder

--- app/src/main/java/models/Contact.java
+++ app/src/main/java/models/Contact.java
@@ -6,7 +6,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonArrayRequest;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,27 +30,30 @@ public class Contact {
     public static void injectContactsFromCloud(final QueueUtils.QueueObject o,
                                                final ArrayList<Contact> contacts,
                                                final MainActivity _interface) {
-        String url = "https://fipo.equisd.com/api/users.json";
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        String url = "https://jsonplaceholder.typicode.com/users";
+        System.out.println("***" + url);
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
                     @Override
-                    public void onResponse(JSONObject response) {
-                        if (response.has("data")) {
+                    public void onResponse(JSONArray response) {
+                        System.out.println("*** respuesta:" + response );
+                            System.out.println("*** cumple" );
                             try {
-                                JSONArray list = response.getJSONArray("data");
+                                JSONArray list = response;
                                 for (int i=0; i < list.length(); i++) {
                                     JSONObject o = list.getJSONObject(i);
                                     contacts.add(new Contact(o.getString("name"),  o.getString("email")));
                                 }
                             } catch (JSONException e) {
                                 e.printStackTrace();
+                                System.out.println("*** " + e );
                             }
                             _interface.refreshList();
-                        }
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        System.out.println("*** " + error );
                     }
                 });
         o.addToRequestQueue(jsonObjectRequest);
