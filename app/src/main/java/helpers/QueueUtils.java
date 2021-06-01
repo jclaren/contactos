a7d996c6de56efc6acebb5caa2ca8bacb9ed5c47
Joaquin Claren <joaquin.claren@gmail.com>
2021-06-01 16:21:54 -0400

    elimina imagenes

--- app/src/main/java/helpers/QueueUtils.java
+++ app/src/main/java/helpers/QueueUtils.java
@@ -11,24 +11,10 @@ public class QueueUtils {
     private static QueueObject uniqueInstance;
     public static class QueueObject {
         private RequestQueue mRequestQueue;
-        private ImageLoader mImageLoader;
         private static Context mCtx;
         private QueueObject(Context context) {
             mCtx = context;
             mRequestQueue = getRequestQueue();
-            mImageLoader = new ImageLoader(mRequestQueue,
-                    new ImageLoader.ImageCache() {
-                        private final LruCache<String, Bitmap>
-                                cache = new LruCache<String, Bitmap>(20);
-                        @Override
-                        public Bitmap getBitmap(String url) {
-                            return cache.get(url);
-                        }
-                        @Override
-                        public void putBitmap(String url, Bitmap bitmap) {
-                            cache.put(url, bitmap);
-                        }
-                    });
         }
 
         public RequestQueue getRequestQueue() {
@@ -42,9 +28,6 @@ public class QueueUtils {
             getRequestQueue().add(req);
         }
 
-        public ImageLoader getImageLoader() {
-            return mImageLoader;
-        }
     }
 
     public static synchronized QueueObject getInstance(Context context) {
