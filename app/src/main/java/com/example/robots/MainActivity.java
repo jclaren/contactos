e8794037a56d7abad191ab0a234929680f3579d4
Joaquin Claren <joaquin.claren@gmail.com>
2021-06-01 15:40:46 -0400

    restart activity

--- app/src/main/java/com/example/robots/MainActivity.java
+++ app/src/main/java/com/example/robots/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.robots;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     ContactAdapter contactAdapter;
     QueueUtils.QueueObject queue = null;
     ArrayList<Contact> items;
+    FragmentTransaction ft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,16 +40,21 @@ public class MainActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Contact contact = (Contact) parent.getItemAtPosition(position);
                 System.out.println("*** Name: " + contact.name);
-
                 Global.currentContact = contact;
-
-                FragmentTransaction ft;
                 ft = getSupportFragmentManager().beginTransaction();
                 ft.replace(R.id.main , new DetailFragment());
                 ft.commit();
             }
         });
     }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+    }
+
     public void refreshList(){
         if ( contactAdapter!= null ) {
             contactAdapter.notifyDataSetChanged();
--- app/src/main/java/models/Contact.java
+++ app/src/main/java/models/Contact.java
@@ -60,4 +60,5 @@ public class Contact {
                 });
         o.addToRequestQueue(jsonObjectRequest);
     }
+
 }
