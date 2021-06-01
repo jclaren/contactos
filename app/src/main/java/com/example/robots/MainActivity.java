64082167196c0f48a71cb928c4cbbb28b5122e4c
Joaquin Claren <joaquin.claren@gmail.com>
2021-06-01 08:46:49 -0400

    Consume api en onCreate

--- app/src/main/java/com/example/robots/MainActivity.java
+++ app/src/main/java/com/example/robots/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.robots;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import adapters.RobotAdapter;
+import helpers.QueueUtils;
+import models.Robot;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView robotList;
+    RobotAdapter robotAdapter;
+    QueueUtils.QueueObject queue = null;
+    ArrayList<Robot> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        robotList = findViewById(R.id.robotList);
+        queue = QueueUtils.getInstance(this.getApplicationContext());
+        items = new ArrayList<>();
+        Robot.injectContactsFromCloud(queue, items, this);
+        robotAdapter = new RobotAdapter(this, items);
+        robotList.setAdapter(robotAdapter);
+    }
+    public void refreshList(){
+        if ( robotAdapter!= null ) {
+            robotAdapter.notifyDataSetChanged();
+        }
     }
 }
