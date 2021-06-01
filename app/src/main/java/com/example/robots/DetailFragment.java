b8758a8df058174533fe23ff77d5176c5297ca80
Joaquin Claren <joaquin.claren@gmail.com>
2021-06-01 14:03:22 -0400

    Refactor

--- app/src/main/java/com/example/robots/DetailFragment.java
+++ app/src/main/java/com/example/robots/DetailFragment.java
@@ -1,19 +1,12 @@
 package com.example.robots;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.example.robots.Global;
-
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.view.View;
 
 public class DetailFragment extends DialogFragment {
     TextView txt_name;
@@ -23,8 +16,7 @@ public class DetailFragment extends DialogFragment {
         // Required empty public constructor
     }
 
-    // TODO: Rename and change types and number of parameters
-    public static DetailFragment newInstance(String param1, String param2) {
+    public static DetailFragment newInstance() {
         DetailFragment fragment = new DetailFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
